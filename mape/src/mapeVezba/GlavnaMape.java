117190e72d3e434e59229e491517da057aeba4b7
Uros <Uros@DESKTOP-UEMGI6Q>
2021-04-13 20:05:38 +0200

    dodata nova klasa i novi paket dao

--- mape/src/mapeVezba/GlavnaMape.java
+++ mape/src/mapeVezba/GlavnaMape.java
@@ -4,7 +4,7 @@ public class GlavnaMape {
 
 	public static void main(String[] args) {
 	
-		// pozdrav za git opet!
+		// pozdrav za git opet, treci put!
 		
 	System.out.println("Heloooooooo");
 		
