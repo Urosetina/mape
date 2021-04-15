41172b837dc70fabe25ad66881b5b49c450b7db5
Uros <Uros@DESKTOP-UEMGI6Q>
2021-04-15 18:59:30 +0200

    Radili smo: Ispitivanje da li je karakter cifra, kreirali klasu "Osoba" i ponovili kontstruktore

--- mape/src/mapeVezba/GlavnaMape.java
+++ mape/src/mapeVezba/GlavnaMape.java
@@ -1,13 +1,37 @@
 package mapeVezba;
 
+import model.Osoba;
+
 public class GlavnaMape {
 
 	public static void main(String[] args) {
 	
 		// pozdrav za git opet, treci put!
 		
+		
+		/*String adresa = "Knez Mihajlova 5";
+		
+		char poslednjiKarakter = adresa.charAt(adresa.length()-1);
+		boolean daLiJePoslednjiKarakterCifra = Character.isDigit(poslednjiKarakter);
+		if(daLiJePoslednjiKarakterCifra) {System.out.println("Jeste cifra");
+		}else {System.out.println("Nije cifra");}
+		
 	System.out.println("Heloooooooo");
 		
+	
+	Osoba osoba = new Osoba();
+	
+	osoba.setIme("Nenad");
+	osoba.setPrezime("Cvijanovic");
+	osoba.setJmbg("0402987710234");
+	
+	Osoba osoba2 = new Osoba("Pera", "Zdera", "1204987710456");
+	
+	System.out.println("Ime osobe je: " + osoba.getIme());
+	System.out.println("Prezime osobe2 je: " + osoba2.getPrezime());*/
+	
+	
+	
 		
 		
 		
